e42f97c46b59a2706d38f4bfacc5a02f5cdba65e
GUSTAVO <GUSTAVO@GUSTAVO-PC>
2016-05-06 11:14:40 -0300

    Switch case, teste commit

--- src/exercicios/pkgswitch/pkgcase/Exercicio1.java
+++ src/exercicios/pkgswitch/pkgcase/Exercicio1.java
@@ -30,6 +30,7 @@ public class Exercicio1 {
     public static void meses() {
 
         System.out.println("Digite um numero de 1 a 12");
+        System.out.println("TESTEEEEEEEEEE");
 
         int n = leia.nextInt();
         String mes = "";
